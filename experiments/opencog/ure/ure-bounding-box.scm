4ed73d57992b74435428c8d16a0b0534dbef751d
Anatoly Belikov <awbelikov@gmail.com>
2018-09-11 16:59:47 +0300

    example for working with ure

--- experiments/opencog/ure/ure-bounding-box.scm
+++ experiments/opencog/ure/ure-bounding-box.scm
@@ -2,17 +2,49 @@
 (load-from-path "conjunction-rule-base-config.scm")
 
 (Concept "BB1")
+(Concept "BB2")
 (Concept "BoundingBox")
 
 (Inheritance (Concept "BB1") (Concept "BoundingBox"))
+(Inheritance (Concept "BB2") (Concept "BoundingBox"))
 
 (define Herring (Predicate "Herring"))
 (define Red (Predicate "Red"))
 
-(define RedBox1 (Evaluation (stv 0.4 0.9) Red (ConceptNode "BB1")))
+
+(define (redness box color) (cog-new-stv 0.64 0.9))
+
+(define RedBox1
+    (EvaluationLink
+         (GroundedPredicateNode "scm: redness")
+         (ListLink (VariableNode "$X")
+                    Red)
+    )
+)
+
+(define RedBox2
+   (EvaluationLink (stv 0.56 0.7)
+         Red
+         (ConceptNode "BB2")
+   )
+)
+
 (define HerringBox1 (Evaluation (stv 0.3 0.8) Herring (ConceptNode "BB1")))
-(define HerringAndRedBox1 (And RedBox1 HerringBox1))
+(define HerringBox2 (Evaluation (stv 0.3 0.8) Herring (ConceptNode "BB2")))
 
-(define result (conj-bc HerringAndRedBox1))
+(define HerringAndRedBox1 (AndLink RedBox1 HerringBox1))
+(define HerringAndRedBox2 (AndLink RedBox2 HerringBox2))
+   
 
-(display (cog-tv (car (cog-outgoing-set result))))
+(define (run-bc and-box) 
+    (define result (conj-bc and-box))
+    (display "\n")
+    (display result)
+    (display (cog-tv (car (cog-outgoing-set result))))
+    (display "\n")
+)
+
+(ure-logger-set-level! "debug")
+(run-bc HerringAndRedBox1)
+;(run-bc HerringAndRedBox2)
+;(cog-evaluate! RedBox1)
